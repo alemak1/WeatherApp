5fc51f7d68357a175aec1fd05203012cf1c9dafd
yongrenwang <czie.alexmak@gmail.com>
2017-02-02 18:16:22 +0800

    Added a file for data model

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,13 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Aleksander Makedonski on 2/2/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather{
+    
+}
